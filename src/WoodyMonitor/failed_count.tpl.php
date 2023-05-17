eb63d628383dbc397f32505e5008bc9d5451cb27
Léo POIROUX <leo.poiroux@gmail.com>
2023-05-17 17:18:02 +0200

    [version] 1.5.6

--- src/WoodyMonitor/failed_count.tpl.php
+++ src/WoodyMonitor/failed_count.tpl.php
@@ -1,5 +1,6 @@
 <?php
 
+header('X-VC-TTL: 0');
 header('Content-Type: text/plain; charset=UTF-8');
 foreach ($data['sites'] as $site) {
     print sprintf('wp_failed_count{database="%s"} %s', $site['site_key'], $site['failed']) . "\n";
